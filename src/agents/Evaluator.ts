264d039f9a8d9c8864c8fb0fb6cb6feaeba5bb63
dtellz <dtellez95@gmail.com>
2023-06-22 20:23:12 +0100

    update deprecated imports

--- src/agents/Evaluator.ts
+++ src/agents/Evaluator.ts
@@ -3,7 +3,8 @@ import { exec } from "child_process";
 import { promisify } from "util";
 
 // LangChain
-import { LLMChain, PromptTemplate } from "langchain";
+import { LLMChain } from "langchain/chains";
+import { PromptTemplate } from "langchain/prompts";
 import { OpenAI } from "langchain/llms/openai";
 
 //Relative imports
@@ -18,71 +19,71 @@ import { VectorMemory } from "../memory";
  * check for common code smells
  */
 export class Evaluator {
-  private chain?: LLMChain;
+    private chain?: LLMChain;
 
-  /**
-   * To be called right after creating the instance.
-   * Prepares the evaluation LLM chain
-   */
-  public async init() {
-    const memory = new VectorMemory();
-    await memory.init();
-    const { openAIApiKey } = await SecretsManager.getInstance().getSecrets();
-    const model = new OpenAI({ openAIApiKey });
-    const prompt =
-      PromptTemplate.fromTemplate(`You are an AI assistant designed to output test execution commands without any other information.
+    /**
+     * To be called right after creating the instance.
+     * Prepares the evaluation LLM chain
+     */
+    public async init() {
+        const memory = new VectorMemory();
+        await memory.init();
+        const { openAIApiKey } = await SecretsManager.getInstance().getSecrets();
+        const model = new OpenAI({ openAIApiKey });
+        const prompt =
+            PromptTemplate.fromTemplate(`You are an AI assistant designed to output test execution commands without any other information.
 
                 Context:
                 {history}
 
                 Current task: {input}`);
 
-    this.chain = new LLMChain({
-      llm: model,
-      prompt,
-      memory: memory.getStore(),
-    });
-  }
-
-  public async depurateCommand(rawCommand: string) {
-    const commandWithoutStopwords = stopwords.reduce((code, stopword) => {
-      return code.split(stopword).join("");
-    }, rawCommand);
-    return commandWithoutStopwords.replace(/\s+/g, " ").trim();
-  }
-
-  /**
-   * Asks the LLM the appropiate command to run the test.
-   *
-   * Spawns a subprocess to run the test and registers the output
-   */
-  public async executeTest(testPath: string): Promise<void> {
-    if (!this.chain) {
-      throw new Error(
-        "Please call Evaluator.init() before Evaluator.executeTest()"
-      );
+        this.chain = new LLMChain({
+            llm: model,
+            prompt,
+            memory: memory.getStore(),
+        });
     }
 
-    const task = `Tell me the begining of the command to run the tests on this project`;
-
-    const result = await this.chain.call({
-      input: task,
-    });
-
-    const rawCommand = result.text;
-
-    const clearCommand = this.depurateCommand(rawCommand);
-    const syncExec = promisify(exec);
-
-    try {
-      const { stdout, stderr } = await syncExec(`${clearCommand} ${testPath}`);
-      console.log("stdout", stdout);
-      console.log("stderr", stderr);
-    } catch (e) {
-      // invalid command
-      // tell the LLM the command failed and ask it for a new command
-      // maybe in a small (controlled) loop until the command success?
-      console.log(e);
+    public async depurateCommand(rawCommand: string) {
+        const commandWithoutStopwords = stopwords.reduce((code, stopword) => {
+            return code.split(stopword).join("");
+        }, rawCommand);
+        return commandWithoutStopwords.replace(/\s+/g, " ").trim();
     }
-  }
+
+    /**
+     * Asks the LLM the appropiate command to run the test.
+     *
+     * Spawns a subprocess to run the test and registers the output
+     */
+    public async executeTest(testPath: string): Promise<void> {
+        if (!this.chain) {
+            throw new Error(
+                "Please call Evaluator.init() before Evaluator.executeTest()"
+            );
+        }
+
+        const task = `Tell me the begining of the command to run the tests on this project`;
+
+        const result = await this.chain.call({
+            input: task,
+        });
+
+        const rawCommand = result.text;
+
+        const clearCommand = this.depurateCommand(rawCommand);
+        const syncExec = promisify(exec);
+
+        try {
+            const { stdout, stderr } = await syncExec(`${clearCommand} ${testPath}`);
+            console.log("stdout", stdout);
+            console.log("stderr", stderr);
+        } catch (e) {
+            // invalid command
+            // tell the LLM the command failed and ask it for a new command
+            // maybe in a small (controlled) loop until the command success?
+            console.log(e);
+        }
+    }
 }
